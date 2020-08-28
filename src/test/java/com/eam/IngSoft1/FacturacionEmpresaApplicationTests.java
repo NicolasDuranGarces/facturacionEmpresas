ba96d2a1d7495262de47ec0c24c67b5f13d492be
papugarces <nicolas.duran.1721@eam.edu.co>
2020-08-27 19:49:20 -0500

    Generacion de Esquelo Del Proyecto en el apartado del backend
    Genercion de la estructura del proyecto e importacion de liberiras en el archivo de pom.xml

---
+++ src/test/java/com/eam/IngSoft1/FacturacionEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eam.IngSoft1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
