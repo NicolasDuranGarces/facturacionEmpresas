4c6ff356f33cc2ce6d387c5981b81686a8b9df9c
papugarces <nicolas.duran.1721@eam.edu.co>
2020-09-05 22:48:33 -0500

    arreglo Domain

--- src/main/java/com/eam/IngSoft1/domain/Empleado.java
+++ src/main/java/com/eam/IngSoft1/domain/Empleado.java
@@ -3,12 +3,15 @@ package com.eam.IngSoft1.domain;
 import java.io.Serializable;
 import javax.persistence.*;
 
+import lombok.Data;
+
 
 /**
  * The persistent class for the empleado database table.
  * 
  */
 @Entity
+@Data
 @NamedQuery(name="Empleado.findAll", query="SELECT e FROM Empleado e")
 public class Empleado implements Serializable {
 	private static final long serialVersionUID = 1L;
