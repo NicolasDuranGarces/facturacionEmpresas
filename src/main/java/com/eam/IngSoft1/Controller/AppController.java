afee67f9ecb5cba493d68de8870dd7151f8088b0
Papu Garces <niduga@outlook.es>
2020-11-08 21:47:06 -0500

    Revert "Adjunto Consultas JPQL Para la Generacion de Facturas y Carrito de Compras"
    This reverts commit 1323529bdd62abbf042d2584899b02b7af9fa0da.

--- src/main/java/com/eam/IngSoft1/Controller/AppController.java
+++ src/main/java/com/eam/IngSoft1/Controller/AppController.java
@@ -1,10 +1,15 @@
 package com.eam.IngSoft1.Controller;
 
+import java.text.ParseException;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 /*
 import co.com.eam.avanzada.domain.Usuario;
 import co.com.eam.avanzada.repository.IUsuarioRepository;
